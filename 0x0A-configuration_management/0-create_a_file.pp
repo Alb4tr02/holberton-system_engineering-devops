e3932c89f8d5f9a5ef9e285d087a4119ec90ec21
Alb4Tr02 <834@holbertonschool.com>
2020-01-10 12:48:29 -0500

    0-file update

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# script to create a file
+
+file {'holberton':
+  path    => '/tmp/holberton',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner    => 'www-data',
+  group   => 'www-data',
+}
