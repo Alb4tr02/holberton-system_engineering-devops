28db35f307b532b946213279a3f1a28fc6a7f50f
Alb4Tr02 <834@holbertonschool.com>
2020-01-15 02:55:01 -0500

    2-puppet_custom_http_response-header.pp fix(?)

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,15 +1,5 @@
 #config server
 
-package { 'nginx':
-provider => 'apt',
-}
-exec {'hlbtn_page':
-command => '/usr/bin/sudo /bin/echo Holberton School > /var/www/html/index.nginx-debian.html',
-}
-exec {'redirect_page':
-
-command => '/usr/bin/sudo /bin/sed -i "66i rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default',
-}
 exec {'add_header':
 
 command => '/usr/bin/sudo /bin/sed -i "60i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
