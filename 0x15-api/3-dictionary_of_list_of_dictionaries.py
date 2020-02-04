550292a6d292618a0663f2b91c2141bcb106bf0b
Alb4Tr02 <834@holbertonschool.com>
2020-02-03 23:42:31 -0500

    3-dictionary_of_list_of_dictionaries.py update

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""  Python script that takes in a URL, sends a request to the URL and displays
+     the body of the response. """
+
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    req = requests.get(url)
+    jjson = req.json()
+    my_file = open("todo_all_employees.json", 'w')
+    all_data = {}
+    for person in jjson:
+        query = person['id']
+        name = person['username']
+        url = "https://jsonplaceholder.typicode.com/todos/"
+        req = requests.get(url, params={"userId": query})
+        jjson = req.json()
+        l = []
+        for task in jjson:
+            l.append({"task": task['title'], "completed": task['completed'],
+                      "username": name})
+        all_data = {query: l}
+    with my_file:
+        json.dump(all_data, my_file)
