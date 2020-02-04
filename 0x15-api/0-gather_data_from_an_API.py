93e2afb826b3e99bd4d91c8857bb0a08f693d0f6
Alb4Tr02 <834@holbertonschool.com>
2020-02-03 22:33:31 -0500

    0-gather_data_from_an_API.py update

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""  Python script that takes in a URL, sends a request to the URL and displays
+     the body of the response. """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    query = sys.argv[1]
+    url = url+query
+    req = requests.get(url)
+    json = req.json()
+    name = json['name']
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    req = requests.get(url, params = {"userId":query})
+    json = req.json()
+    total = len(json)
+    comp = ""
+    n_comp = 0
+    for task in json:
+        if (task['completed'] == True):
+            comp = comp + "\t " + task['title'] + "\n"
+            n_comp = n_comp + 1
+    print("Employee {} is done with tasks({}/{}):".format(name, n_comp, total))
+    print(comp, end = "")
