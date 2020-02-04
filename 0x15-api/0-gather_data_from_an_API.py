548b48e2603927d19450998e7a097e8f737b195a
Alb4Tr02 <834@holbertonschool.com>
2020-02-03 22:38:05 -0500

    0-gather_data_from_an_API.py fix pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     json = req.json()
     name = json['name']
     url = "https://jsonplaceholder.typicode.com/todos/"
-    req = requests.get(url, params = {"userId":query})
+    req = requests.get(url, params={"userId": query})
     json = req.json()
     total = len(json)
     comp = ""
     n_comp = 0
     for task in json:
-        if (task['completed'] == True):
+        if (task['completed']):
             comp = comp + "\t " + task['title'] + "\n"
             n_comp = n_comp + 1
     print("Employee {} is done with tasks({}/{}):".format(name, n_comp, total))
-    print(comp, end = "")
+    print(comp, end="")
