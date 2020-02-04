b39c92e18afc7828bd098934571b4723c4d38884
Alb4Tr02 <834@holbertonschool.com>
2020-02-03 23:10:48 -0500

    1-export_to_CSV.py fix extension

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/todos/"
     req = requests.get(url, params={"userId": query})
     json = req.json()
-    my_file = open(query+".cvs", 'w')
+    my_file = open(query+".csv", 'w')
     with my_file:
         writer = csv.writer(my_file, quoting=csv.QUOTE_ALL)
         for task in json:
