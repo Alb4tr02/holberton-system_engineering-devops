290e7992cb52ed85c593814f289c78f195ab0309
Alb4Tr02 <834@holbertonschool.com>
2020-02-06 18:38:58 -0500

    2-recurse.py ninja mode

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,10 +8,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], nexT=""):
     headers = {'User-agent': 'Alb4tr02'}
-    if (len(nexT) != 0):
-        url = "https://www.reddit.com/r/"+subreddit+"/hot/.json?after="+nexT
-    else:
-        url = "https://www.reddit.com/r/"+subreddit+"/hot/.json"
+    url = "https://www.reddit.com/r/"+subreddit+"/hot/.json"+nexT
     req = requests.get(url, headers=headers)
     json = req.json()
     if ('error' in json.keys()):
@@ -19,6 +16,6 @@ def recurse(subreddit, hot_list=[], nexT=""):
     for post in json['data']['children']:
         hot_list.append(post['data']['title'])
     if (json['data']['after'] is not None):
-        return recurse(subreddit, hot_list, json['data']['after'])
+        return recurse(subreddit, hot_list, "?after="+json['data']['after'])
     else:
         return hot_list
