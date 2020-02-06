900f9f0af01b28be870308e1e2f36dc5bc22aac5
Alb4Tr02 <834@holbertonschool.com>
2020-02-06 17:56:54 -0500

    1-top_ten.py update

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""  Python script that takes in a URL, sends a request to the URL and displays
+     the body of the response. """
+
+import requests
+
+
+def top_ten(subreddit):
+    headers = {'User-agent': 'Alb4tr02'}
+    url = "https://www.reddit.com/r/"+subreddit+"/hot/.json"
+    req = requests.get(url, headers=headers, params={'limit': 10})
+    json = req.json()
+    if ('error' in json.keys()):
+        return 0
+    for post in json['data']['children']:
+        print(post['data']['title'])
